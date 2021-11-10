cd72eee3e5d50d7e805051c9fc8c5fd90e8fc7a0
truengsuksilp <truengsuksilp@gmail.com>
2021-11-09 20:55:45 -0800

    Signup Page: Extended Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Extends user model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500)
+    location = models.CharField(max_length=30)
+    birth_date = models.DateField()
--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,8 @@ from django.views.generic.base import TemplateView
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+# Auth
+from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
 
 # Create your views here.
@@ -18,13 +20,19 @@ class SignUp(View):
 
     def post(self, request):
         form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
+        birth_date = request.POST.get('birth_date')
 
+        if form.is_valid():
+            user=form.save()
+            profile = Profile.objects.create(user=user, birth_day=birth_day)
+            
+            # Authenticate
+            # username = form.
+            # cleaned_data.get('username')
+            # raw_password = form.cleaned_data.get('password1')
+            # user = authenticate(username=username, password=password)
+
+            login(request, user)
             return redirect('home')
         else: 
             context = {'form': form}
